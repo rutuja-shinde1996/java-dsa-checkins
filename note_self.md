5fffe08d7a58cf880d3a2e57acb9efbb36bb9d0f
Rutuja Shinde <rutujashinde1696@gmail.com>
2026-06-26 21:19:08 +0530

    Initial Structure

---
+++ note_self.md
@@ -0,0 +1,20 @@
+Learning Order
+---------------------
+Arrays
+Strings
+Hashing
+Two Pointers
+Sliding Window
+Binary Search
+Linked List
+Stack
+Queue
+Trees
+BST
+Heap
+Graphs
+Backtracking
+Greedy
+Dynamic Programming
+Trie
+Bit Manipulation
---
+++ src/template.java
@@ -0,0 +1,24 @@
+/*
+Problem:
+https://leetcode.com/problems/two-sum/
+
+Difficulty:
+Easy
+
+Topics:
+Array, HashMap
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(n)
+*/
+
+import java.util.*;
+
+public class template {
+
+    public void function() {
+    }
+}
